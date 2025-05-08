f710ef73d8aa684598a08e6e2e71592ea7265458
Pedro Silva <DarthPedro@gmail.com>
2025-05-08 11:53:00 -0700

    Added basic MinimalApi project with default Forecast endpoint.

---
+++ samples/MemberService/Endpoints/Forecasts/WeatherForecastEndpoints.cs
@@ -0,0 +1,21 @@
+﻿namespace MemberService.Endpoints.Forecasts;
+
+internal static class WeatherForecastEndpoints
+{
+    private static readonly string[] _summaries =
+    [
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    ];
+
+    public static void MapForecastEndpoints(this IEndpointRouteBuilder routes) =>
+        routes.MapGet("api/v1/weatherforecast", static () =>
+            Enumerable.Range(1, 5)
+                      .Select(index =>
+                new WeatherForecast
+                (
+                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                    Random.Shared.Next(-20, 55),
+                    _summaries[Random.Shared.Next(_summaries.Length)]
+                )).ToArray()
+        ).WithName("GetWeatherForecast");
+}
---
+++ samples/MemberService/Program.cs
@@ -0,0 +1,21 @@
+using MemberService.Endpoints.Forecasts;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.MapForecastEndpoints();
+app.Run();
