cc101d470a371ecc525172c921f28c2fb60a8e2e
Pedro Silva <DarthPedro@gmail.com>
2025-05-08 12:04:00 -0700

    Introduced DependencyInjection class to simplify program configuration and startup. Added references to Mediator project and Functional.AspNetCore package.

---
+++ samples/MemberService/DependencyInjection.cs
@@ -0,0 +1,32 @@
+﻿using MemberService.Endpoints.Forecasts;
+
+namespace MemberService;
+
+internal static class DependencyInjection
+{
+    public static WebApplicationBuilder ConfigureServices(this WebApplicationBuilder builder)
+    {
+        // Add services to the container.
+
+        // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+        builder.Services.AddOpenApi();
+
+        return builder;
+    }
+
+    public static WebApplication ConfigurePipeline(this WebApplication app)
+    {
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.MapOpenApi();
+        }
+
+        app.UseHttpsRedirection();
+
+        // Map endpoints
+        app.MapForecastEndpoints();
+
+        return app;
+    }
+}
--- samples/MemberService/Program.cs
+++ samples/MemberService/Program.cs
@@ -1,21 +1,7 @@
-using MemberService.Endpoints.Forecasts;
+using MemberService;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
-
-app.UseHttpsRedirection();
-
-app.MapForecastEndpoints();
-app.Run();
+WebApplication.CreateBuilder(args)
+              .ConfigureServices()
+              .Build()
+              .ConfigurePipeline()
+              .Run();
