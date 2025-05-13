8571f4ff973d4104d348272b1d7dcb37b04a5efb
Pedro Silva <DarthPedro@gmail.com>
2025-05-13 16:36:38 -0700

    Updated MemberEndpoints operations to get cancellation token from the system and pass it through the Mediator calls.

--- samples/MemberService/Endpoints/Members/MemberEndpoints.cs
+++ samples/MemberService/Endpoints/Members/MemberEndpoints.cs
@@ -12,22 +12,24 @@ internal static class MemberEndpoints
         var group = routes.MapGroup("api/v1/members")
                           .WithTags("Member Service");
 
-        group.MapGet("/", async (IMediator mediator) =>
-                 await mediator.SendAsync(new GetAllMembers.Command())
+        group.MapGet("/", async (IMediator mediator, CancellationToken token) =>
+                 await mediator.SendAsync(new GetAllMembers.Command(), token)
                                 .ToApiResultAsync())
              .Produces<MemberResponse[]>()
              .WithName("GetAllMembers");
 
-        group.MapGet("/{id:int}", async ([FromServices] IMediator mediator, [FromRoute] int id) =>
-                 await mediator.SendAsync(new GetMemberById.Command(id))
+        group.MapGet("/{id:int}",
+             async ([FromServices] IMediator mediator, [FromRoute] int id, CancellationToken token) =>
+                 await mediator.SendAsync(new GetMemberById.Command(id), token)
                                .ToApiResultAsync())
              .Produces<MemberResponse>(StatusCodes.Status200OK)
              .ProducesProblem(StatusCodes.Status404NotFound)
              .ProducesValidationProblem(StatusCodes.Status400BadRequest)
              .WithName("GetMemberById");
 
-        group.MapGet("/email/{email}", async ([FromServices] IMediator mediator, [FromRoute] string email) =>
-                 await mediator.SendAsync(new GetMemberByEmail.Command(email))
+        group.MapGet("/email/{email}", 
+             async ([FromServices] IMediator mediator, [FromRoute] string email, CancellationToken token) =>
+                 await mediator.SendAsync(new GetMemberByEmail.Command(email), token)
                                .ToApiResultAsync())
              .Produces<MemberResponse>(StatusCodes.Status200OK)
              .ProducesProblem(StatusCodes.Status404NotFound)
@@ -35,8 +37,8 @@ internal static class MemberEndpoints
              .WithName("GetMemberByEmail");
 
         group.MapPost("/",
-            async ([FromServices] IMediator mediator, [FromBody] CreateMemberRequest request) =>
-                await mediator.SendAsync(request.Map())
+             async ([FromServices] IMediator mediator, [FromBody] CreateMemberRequest request, CancellationToken token) =>
+                await mediator.SendAsync(request.Map(), token)
                               .IterAsync(async response => await mediator.NotifyAsync(
                                   new MemberCreatedNotification(response.FirstName, response.Email, response.CellPhone)))
                               .MatchAsync(
@@ -48,16 +50,17 @@ internal static class MemberEndpoints
              .WithName("CreateMember");
 
         group.MapPut("/{id:int}",
-            async ([FromServices] IMediator mediator, [FromBody] UpdateMemberRequest request, [FromRoute] int id) =>
-                await mediator.SendAsync(request.Map(id))
+             async ([FromServices] IMediator mediator, [FromBody] UpdateMemberRequest request, [FromRoute] int id, CancellationToken token) =>
+                await mediator.SendAsync(request.Map(id), token)
                               .ToApiResultAsync())
              .Produces<MemberResponse>(StatusCodes.Status200OK)
              .ProducesProblem(StatusCodes.Status404NotFound)
              .ProducesValidationProblem(StatusCodes.Status400BadRequest)
              .WithName("UpdateMember");
 
-        group.MapDelete("/{id:int}", async ([FromServices] IMediator mediator, [FromRoute] int id) =>
-                 await mediator.SendAsync(new DeleteMember.Command(id))
+        group.MapDelete("/{id:int}",
+             async ([FromServices] IMediator mediator, [FromRoute] int id, CancellationToken token) =>
+                 await mediator.SendAsync(new DeleteMember.Command(id), token)
                                .ToApiResultAsync())
              .Produces<MemberResponse>(StatusCodes.Status200OK)
              .ProducesProblem(StatusCodes.Status404NotFound)
